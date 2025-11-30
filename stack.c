919c1286836cb7540a98f3a315117aeedc64db17
eleonoraSt <eas402me@gmail.com>
2025-11-30 11:45:37 +0300

    Stack structure with basic funcs and several additional funcs for usage in sorting

---
+++ stack.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include "stack.h"
+
+short createStack(int val, Stack **stack) {
+    // Создаёт стек по адресу stack с первым элементом val, возвращает код ошибки
+    *stack = malloc(sizeof(Stack));
+    if (*stack == NULL) {
+        return MEMORY_ERR;
+    }
+    (*stack)->p = malloc(sizeof(Elem));
+    if ((*stack)->p == NULL) {
+        free(*stack);
+        return MEMORY_ERR;
+    }
+    (*stack)->p->val = val;
+    (*stack)->p->link = NULL;
+    (*stack)->top = stack->p;
+    return OK;
+}
+
+short addElem(int val, Stack *stack) {
+    // Добавляет в стек по адресу stack элемент val, возвращает код ошибки
+    stack->p = malloc(sizeof(Elem));
+    if (stack->p == NULL) {
+        return MEMORY_ERR;
+    }
+    stack->p->val = val;
+    stack->p->link = stack->top;
+    stack->top = stack->p;
+    return OK;
+}
+
+int deleteElem(Stack *stack) {
+    // Удаляет элемент из стека по адресу stack
+    int val = stack->top->val;
+    stack->p = stack->top;
+    stack->top = stack->top->link;
+    free(stack->p);
+    return val;
+}
+
+void deleteStack(Stack *stack) {
+    // Удаляет стек и освобождает всё
+    while (stack->top != NULL) {
+        deleteElem(stack);
+    }
+    free(stack);
+}
+
+// Ввести проверку на переполнение переменной с длинной. Или защиту от чрезмерной длины
+int stackLength(Stack *stack) {
+    int length = 1;
+    Elem *current = stack->top;
+    while (current->link != NULL) {
+        length++;
+        current = current->link;
+    }
+    return length;
+}
+
+short moveStack(Stack *source, Stack *goal, int size) {
+    if (createStack(deleteElem(source), &goal) == MEMORY_ERR){
+        return MEMORY_ERR;
+    }
+    for (int i = 1; i < size; i++) {
+        if (addElem(deleteElem(source), goal) == MEMORY_ERR) {
+            deleteStack(goal);
+            return MEMORY_ERR;
+        }
+    }
+    return OK;
+}
---
+++ stack.h
@@ -0,0 +1,33 @@
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+
+typedef struct Elem {
+    int val;
+    struct Elem *link;
+} Elem;
+
+typedef struct {
+    Elem *top;
+    Elem *p;
+} Stack;
+
+enum ERRORS {
+    OK = 0,
+    MEMORY_ERR = 1,
+    INPUT_ERR = 2,
+    INPUT_END = 3
+};
+
+short createStack(int val, Stack **stack);
+
+short addElem(int val, Stack *stack);
+
+int deleteElem(Stack *stack);
+
+void deleteStack(Stack *stack);
+
+int stackLength(Stack *stack);
+
+short moveStack(Stack *source, Stack *goal, int size);
+
+#endif // STACK_H_INCLUDED
